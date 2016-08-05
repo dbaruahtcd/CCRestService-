9ef884348506f97dd48642fba12da42e31bb7bfe
Dan <baruahd@tcd.ie>
2016-08-05 20:21:28 +0100

    changed  the git url paramter

--- org/scss/jgit/helper/FixedParams.java
+++ org/scss/jgit/helper/FixedParams.java
@@ -5,6 +5,7 @@ public final class FixedParams {
 	private static final String REMOTE_URL = "https://github.com/facebook/css-layout.git";
 	private static final String LOCAL_GIT_DIR = "C:/Users/Dan/Desktop/code optimization papers/code/facebook-java-ads-sdk/.git";
 	protected static final String FILE_PATTERN = ".*(\\.js.*)|(.*\\.cpp.*)|(.*\\.py.*)|(.*\\.c)|(.*\\.rb)|(.*\\.php)|(.*\\.java)|(.*\\.sh)|(.*\\.ml)|(.*\\.hs)"; // hs haskell ml - ocaml
+	private static String projectUrl = "";
 	//private static final String FILE_PATTERN1 = ".*(\\.js.*)|(.*\\.cpp.*)|(.*\\.py.*)|(.*\\.c)|(.*\\.rb)|(.*\\.php)|(.*\\.java)";
 
 
@@ -20,6 +21,17 @@ public final class FixedParams {
 	public static String getFilePattern()
 	{
 		return FILE_PATTERN;
+		
+	}
+	
+	public String getProjectUrl()
+	{ 
+		return projectUrl;
+	}
+	
+	public static void setProjectUrl(String url)
+	{
+		projectUrl = url;
 	}
 
 }
